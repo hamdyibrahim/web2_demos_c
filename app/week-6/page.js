9f936642f50a294a7617c4cab821328fcf311094
Hamdy Ibrahim <ha_madien@yahoo.com>
2025-10-08 15:55:45 -0600

    adding conditional rendering

--- app/week-6/page.js
+++ app/week-6/page.js
@@ -54,7 +54,7 @@ export default function Page() {
           </li>
         ))}
       </ul>
-      <div>You selected {selectedDogId}</div>
+      {selectedDogId && <div>You selected {selectedDogId}</div>}
     </main>
   );
 }
